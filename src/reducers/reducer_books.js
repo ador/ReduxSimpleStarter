6288d066db21f62ce89a369dcc96a9a5f44fb86b
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2017-01-23 11:56:48 +0100

    adding a book list -- but it is not connected yet

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function() {
+  return [
+    {title: 'book title #1'},
+    {title: 'book title #2'},
+    {title: 'book title #3'},
+    {title: 'book title #4'},
+    {title: 'book title #5'},
+    {title: 'book title #6'}
+  ]
+
+}
