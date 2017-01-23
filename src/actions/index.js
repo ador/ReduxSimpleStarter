74acf318765d5aa20faaa2596e01dd657604db23
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2017-01-23 15:23:37 +0100

    introducing actions - returning a proper object with type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-  console.log('Book selected: ', book.title);
+  // should return an object with a type
+  console.log('click! on ' , book.title);
+  return {
+    type:'BOOK_SELECTED',
+    payload: book
+  }
 }
