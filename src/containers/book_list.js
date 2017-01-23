b09d84f0ae8b20cdca694ef08cb8cf82296516f2
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2017-01-23 15:19:47 +0100

    introducing actions - wip - fix

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -8,7 +8,11 @@ class BookList extends Component {
   renderList() {
     return this.props.books.map(
       (book) => { return(
-         <li key={book.title} className="list-group-item" > {book.title} </li>
+          <li key={book.title}
+              onClick={()=> this.props.selectBook(book)}
+              className="list-group-item">
+            {book.title}
+          </li>
         )}
     )
   }
@@ -44,4 +48,4 @@ function mapDispatchToProps(dispatch) {
 
 
 
-export default connect(mapStateToProps, mapDispathcToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
