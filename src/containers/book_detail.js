369b46671d5c3c720dbbccb96c6654e5a0354f25
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2017-01-24 15:01:35 +0100

    bookdetail connected, but error: tries to render null (default value before a first click)

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-
-export default class BookDetail extends Component {
+class BookDetail extends Component {
   render() {
     return (
       <div>
-        Book detail
+        <h3>Book detail </h3>
+        <div> {this.props.book.title} </div>
       </div>
     );
   }
 }
+
+
+function mapStateToProps(state) {
+  // what object we return from here will be the "props" of the BookList
+  return {
+    book: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
