ce70c37e1397a73d68ba03c107f3347404b2c8d6
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2017-01-25 12:41:44 +0100

    new example: weather forecasts

--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-export function selectBook(book) {
-  // should return an object with a type
-  console.log('click! on ' , book.title);
-  return {
-    type:'BOOK_SELECTED',
-    payload: book
-  }
-}
