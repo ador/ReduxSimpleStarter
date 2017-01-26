880c9bd17071e8ed414be2a6bb4805ec3a3cff96
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2017-01-26 13:51:50 +0100

    installed axios, using it to query data from an API

--- weathers_example/src/actions/index.js
+++ weathers_example/src/actions/index.js
@@ -1,2 +1,18 @@
-const API_KEY = 'b384301ba16a55db5ced6f60e7821f0f';
+import axios from 'axios';
 
+const API_KEY = 'b384301ba16a55db5ced6f60e7821f0f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&${city},us`;
+  // a promise
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
