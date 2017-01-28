b3bcfed2a32a5880c1d1cd8119016e61abc5b3ee
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2017-01-28 10:21:43 +0100

    dealing with the weather api response - wip (redux-promise middleware does its magic)

--- weathers_example/src/actions/index.js
+++ weathers_example/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'b384301ba16a55db5ced6f60e7821f0f';
+const API_KEY = 'b384301ba16a55db5ced6f60e7821f0e';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 
@@ -10,6 +10,7 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   // a promise
   const request = axios.get(url);
+  console.log("Request in action creator: ", request);
 
   return {
     type: FETCH_WEATHER,
---
+++ weathers_example/src/reducers/reducer_weather.js
@@ -0,0 +1,5 @@
+export default function(state = null, action) {
+  // temporary implementation
+  console.log(' - action received in weather reducer: ', action);
+  return state;
+}
