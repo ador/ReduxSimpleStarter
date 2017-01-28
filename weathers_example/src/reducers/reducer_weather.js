a92c22d43e377c7e77b4b7e030dd73d0e46ab8bb
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2017-01-28 10:36:35 +0100

    finishing weather reducer

--- weathers_example/src/reducers/reducer_weather.js
+++ weathers_example/src/reducers/reducer_weather.js
@@ -1,5 +1,11 @@
-export default function(state = null, action) {
-  // temporary implementation
-  console.log(' - action received in weather reducer: ', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [action.payload.data].concat(state);
+      // another, more modern sytax for the same as above:
+      //return [action.payload.data, ...state ] 
+  }
   return state;
 }
